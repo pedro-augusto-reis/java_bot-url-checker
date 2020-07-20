63d72dec23712d1a4fa07cf8048dc9c3e0869078
Pedro Reis <pdroreis@gmail.com>
2020-07-20 15:51:06 -0300

    Ajuste SonarLint

--- src/main/java/principal/Main.java
+++ src/main/java/principal/Main.java
@@ -8,21 +8,20 @@ import java.util.Properties;
 
 public class Main {
 
-    private final String TEXTO_PESQUISA = "";
-    private final String URL = "";
-    private final String EMAIL_HOST = "";
-    private final String EMAIL_PORT = "";
-    private final String EMAIL_USERNAME = "";
-    private final String EMAIL_PASSWORD = "";
-    private final String EMAIL_DESTINATARIO = "";
-    private final String EMAIL_ASSUNTO = "";
-    private final String EMAIL_MENSAGEM = "";
-
+    private static final String TEXTO_PESQUISA = "";
+    private static final String URL = "";
+    private static final String EMAIL_HOST = "";
+    private static final String EMAIL_PORT = "";
+    private static final String EMAIL_USERNAME = "";
+    private static final String EMAIL_PASSWORD = "";
+    private static final String EMAIL_DESTINATARIO = "";
+    private static final String EMAIL_ASSUNTO = "";
+    private static final String EMAIL_MENSAGEM = "";
 
     private String response = "";
 
-    public static void main(String[] args) throws InterruptedException {
-        Main main = new Main();
+    public static void main(final String[] args) throws InterruptedException {
+        final Main main = new Main();
         while (true) {
             main.getConteudoPagina().checkConteudoPagina();
             System.out.println("Dormindo 10 segundos ...");
@@ -32,10 +31,10 @@ public class Main {
 
     Main getConteudoPagina() {
         try {
-            response = Unirest.get(URL)
-                    .header("Cookie", "_d2id=7dd94422-41e7-47c8-b3e1-ed593e67ab27-n; navigation_items=MLB1557240039%7C17062020171748")
+            response = Unirest.get(URL).header("Cookie",
+                    "_d2id=7dd94422-41e7-47c8-b3e1-ed593e67ab27-n; navigation_items=MLB1557240039%7C17062020171748")
                     .asString().getBody();
-        } catch (Exception ex) {
+        } catch (final Exception ex) {
             response = "";
             ex.printStackTrace();
         }
@@ -50,31 +49,31 @@ public class Main {
 
     void enviarEmail() {
         System.out.println("Enviando e-mail");
-        Session session;
-        Properties prop = new Properties();
+        
+        final Properties prop = new Properties();
         prop.put("mail.smtp.host", EMAIL_HOST);
         prop.put("mail.smtp.port", EMAIL_PORT);
         prop.put("mail.smtp.auth", "true");
         prop.put("mail.smtp.socketFactory.port", EMAIL_PORT);
         prop.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-        session = Session.getInstance(prop, new javax.mail.Authenticator() {
+        prop.put("mail.smtp.ssl.checkserveridentity","true");
+
+        Session session = Session.getInstance(prop, new javax.mail.Authenticator() {
+            @Override
             protected PasswordAuthentication getPasswordAuthentication() {
                 return new PasswordAuthentication(EMAIL_USERNAME, EMAIL_PASSWORD);
             }
         });
 
-        MimeMessage message = new MimeMessage(session);
+        final MimeMessage message = new MimeMessage(session);
         try {
             message.setFrom(new InternetAddress(EMAIL_USERNAME));
-            message.setRecipients(
-                    Message.RecipientType.TO,
-                    InternetAddress.parse(EMAIL_DESTINATARIO)
-            );
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(EMAIL_DESTINATARIO));
             message.setSubject(EMAIL_ASSUNTO);
             message.setText(EMAIL_MENSAGEM);
 
             Transport.send(message);
-        } catch (Exception ex) {
+        } catch (final Exception ex) {
             ex.printStackTrace();
         }
     }
